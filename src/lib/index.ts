c1d5444f503e6f3d20a4bfc0d6e262c869176ffd
Juunini <juuni.ni.i@gmail.com>
2024-12-29 01:10:40 +0900

    add HemisphericLight component

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ import Scene from '$lib/Scene.svelte';
 import Engine from '$lib/engine/Engine.svelte';
 import FreeCamera from '$lib/camera/FreeCamera.svelte';
 import TargetCamera from '$lib/camera/TargetCamera.svelte';
+import HemisphericLight from '$lib/light/HemisphericLight.svelte';
 
-export { Canvas, Scene, Engine, FreeCamera, TargetCamera };
+export { Canvas, Scene, Engine, FreeCamera, TargetCamera, HemisphericLight };
---
+++ src/lib/light/HemisphericLight.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+	import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+	import type { Scene } from '@babylonjs/core/scene';
+	import { onDestroy, type Snippet } from 'svelte';
+	import { v7 } from 'uuid';
+
+	interface Props {
+		light?: HemisphericLight;
+		name?: string;
+		direction?: Vector3;
+		scene?: Scene;
+		children?: Snippet;
+	}
+
+	let {
+		light = $bindable(),
+		name = $bindable(`light${v7()}`),
+		direction = new Vector3(0, 0, 0),
+		scene,
+		children
+	}: Props = $props();
+
+	light = new HemisphericLight(name, direction, scene);
+
+	onDestroy(() => {
+		light.dispose();
+	});
+</script>
+
+{#if light}
+	{@render children?.()}
+{/if}
