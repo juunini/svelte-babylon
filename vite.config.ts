362d839ef232dc4a516ab5a7c423f1917134ae02
Juunini <juuni.ni.i@gmail.com>
2024-12-29 15:28:35 +0900

    add `manualChunks` in build config for reduce build size

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,37 @@ export default defineConfig({
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					if (id.includes('@babylonjs/core/scene')) {
+						return '@babylonjs/core/scene';
+					}
+
+					if (id.includes('@babylonjs/core/Engines')) {
+						return '@babylonjs/core/Engines';
+					}
+
+					if (id.includes('@babylonjs/core/Materials')) {
+						return '@babylonjs/core/Materials';
+					}
+
+					if (id.includes('@babylonjs/core/types')) {
+						return '@babylonjs/core/types';
+					}
+
+					if (id.includes('@babylonjs/core/Maths')) {
+						return '@babylonjs/core/Maths';
+					}
+
+					if (id.includes('@babylonjs/core/Meshes')) {
+						return '@babylonjs/core/Meshes';
+					}
+				}
+			}
+		}
 	}
 });
