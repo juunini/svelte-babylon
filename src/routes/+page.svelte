96c54f9c59351a8f3e8fbfeff3898f23d9181ade
Juunini <juuni.ni.i@gmail.com>
2024-12-29 23:42:45 +0900

    change default page same to README

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Vector3 } from '@babylonjs/core/Maths/math.vector';
+
+  import Canvas from '$lib/Canvas.svelte';
+  import BabylonEngine from '$lib/engine/Engine.svelte';
+  import BabylonScene from '$lib/Scene.svelte';
+  import BabylonFreeCamera from '$lib/camera/FreeCamera.svelte';
+  import BabylonGround from '$lib/mesh/Ground.svelte';
+  import BabylonHemisphericLight from '$lib/light/HemisphericLight.svelte';
+  import BabylonSphere from '$lib/mesh/Sphere.svelte';
+</script>
+
+<Canvas style="width: 100%;">
+  <BabylonEngine>
+    <BabylonScene>
+      <BabylonFreeCamera position={new Vector3(0, 5, -10)} />
+      <BabylonHemisphericLight direction={new Vector3(0, 1, 0)} intensity={0.7} />
+      <BabylonGround options={{ width: 6, height: 6 }} />
+      <BabylonSphere options={{ diameter: 2, segments: 32 }} position={{ x: 0, y: 1, z: 0 }} />
+    </BabylonScene>
+  </BabylonEngine>
+</Canvas>
