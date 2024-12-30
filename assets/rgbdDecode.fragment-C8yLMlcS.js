15647fb12750a6c8ee07a5aad41a633087be6a17
juunini <juunini@users.noreply.github.com>
2024-12-30 18:55:42 +0000

    deploy: d1c48dfbcd211eacdd87c5e6cf33c192bf453b9a

---
+++ assets/rgbdDecode.fragment-C8yLMlcS.js
@@ -0,0 +1,5 @@
+import{a as t}from"./create-runtime-stories-BkZzGcfe.js";import"./helperFunctions-DC8vOQgA.js";import"./props-Biv-wkS8.js";import"./index-client-rkuF54Lr.js";import"./iframe-B3Qvg23w.js";import"../sb-preview/runtime.js";import"./index-D-8MO0q_.js";import"./index-BHYIh-Xd.js";import"./_commonjsHelpers-Cpj98o6Y.js";const e="rgbdDecodePixelShader",r=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
+#include<helperFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=vec4f(fromRGBD(textureSample(textureSampler,textureSamplerSampler,input.vUV)),1.0);}`;t.ShadersStoreWGSL[e]=r;const d={name:e,shader:r};export{d as rgbdDecodePixelShaderWGSL};
