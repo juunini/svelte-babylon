b7dfad9299c50d3e3f596f1f5425095368f4352c
Juunini <juuni.ni.i@gmail.com>
2024-12-31 03:41:40 +0900

    fix box component storybook

--- src/stories/mesh/Box.stories.svelte
+++ src/stories/mesh/Box.stories.svelte
@@ -1,16 +1,12 @@
 <script lang="ts" module>
   import { defineMeta } from '@storybook/addon-svelte-csf';
   import { Vector3 } from '@babylonjs/core/Maths/math.vector';
-  import type { Mesh } from '@babylonjs/core/Meshes/mesh';
-
   import Canvas from '@juunini/svelte-babylonjs/Canvas.svelte';
   import Engine from '@juunini/svelte-babylonjs/engine/Engine.svelte';
   import Scene from '@juunini/svelte-babylonjs/Scene.svelte';
   import FreeCamera from '@juunini/svelte-babylonjs/camera/FreeCamera.svelte';
   import HemisphericLight from '@juunini/svelte-babylonjs/light/HemisphericLight.svelte';
   import Box from '@juunini/svelte-babylonjs/mesh/Box.svelte';
-
-  let mesh: Mesh;
 
   const { Story } = defineMeta({
     title: 'mesh/Box',
@@ -22,11 +18,10 @@
 <Story
   name="Docs"
   args={{
-    options: { width: 2, height: 2, depth: 2 },
-    position: Vector3.Zero(),
-    lookAt: {
-      targetPoint: Vector3.Zero()
-    }
+    options: { width: 4, height: 4, depth: 4 },
+    position: { x: 0, y: 0, z: 0 },
+    rotation: { x: 0, y: 0, z: 0 },
+    scaling: { x: 1, y: 1, z: 1 }
   }}
 >
   {#snippet children(args)}
@@ -35,7 +30,12 @@
         <Scene>
           <FreeCamera position={new Vector3(0, 5, -10)} />
           <HemisphericLight direction={new Vector3(0, 1, 0)} intensity={0.7} />
-          <Box bind:mesh options={args.options} position={args.position} lookAt={args.lookAt} />
+          <Box
+            options={args.options}
+            position={args.position}
+            rotation={args.rotation}
+            scaling={args.scaling}
+          />
         </Scene>
       </Engine>
     </Canvas>
