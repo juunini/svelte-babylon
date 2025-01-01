6f93550250025d25ab35c7a0ea55f51b2d4daf4d
juunini <juunini@users.noreply.github.com>
2025-01-01 10:06:19 +0000

    deploy: 87d520988b4727bd8c90fd98d6b04e40677fce64

---
+++ assets/meshUboDeclaration-CysEZgCa.js
@@ -0,0 +1,13 @@
+import{S as e}from"./create-runtime-stories-ChL5sEf5.js";const o="sceneUboDeclaration",t=`struct Scene {viewProjection : mat4x4<f32>,
+#ifdef MULTIVIEW
+viewProjectionR : mat4x4<f32>,
+#endif 
+view : mat4x4<f32>,
+projection : mat4x4<f32>,
+vEyePosition : vec4<f32>,};
+#define SCENE_UBO
+var<uniform> scene : Scene;
+`;e.IncludesShadersStoreWGSL[o]=t;const n="meshUboDeclaration",r=`struct Mesh {world : mat4x4<f32>,
+visibility : f32,};var<uniform> mesh : Mesh;
+#define WORLD_UBO
+`;e.IncludesShadersStoreWGSL[n]=r;
